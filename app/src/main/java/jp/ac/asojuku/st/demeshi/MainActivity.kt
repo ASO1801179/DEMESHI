ed1ddcb8db259a877c43792e03eed9cb9c98714d
Ryu <huziie1999@icloud.com>
2019-06-10 10:36:25 +0900

    お試し2

--- app/src/main/java/jp/ac/asojuku/st/demeshi/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/st/demeshi/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         kari.setOnClickListener{startActivity(Intent(it.context,HaveCardList::class.java))}
+
         
     }
 }
