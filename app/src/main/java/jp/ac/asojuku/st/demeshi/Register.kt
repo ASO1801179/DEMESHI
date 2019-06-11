a33f8c051b9883784c3a30da7e6e6089e76dc6f1
Ryu <huziie1999@icloud.com>
2019-06-11 12:16:23 +0900

    アクティビティ変更前

--- app/src/main/java/jp/ac/asojuku/st/demeshi/Register.kt
+++ app/src/main/java/jp/ac/asojuku/st/demeshi/Register.kt
@@ -1,12 +1,15 @@
 package jp.ac.asojuku.st.demeshi
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_register.*
 
 class Register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        LoginBtn.setOnClickListener{startActivity(Intent(it.context,MyCardList::class.java))}
     }
 }
